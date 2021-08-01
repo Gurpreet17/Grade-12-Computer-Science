14a9405856c290a9027961571b44ece86d2e46fc
Gurpreet Singh <74424705+Gurpreet17@users.noreply.github.com>
2021-08-01 13:57:08 -0400

    Add files via upload

---
+++ Unit 4 - Data Structures/Exercise 1 - Arrays/src/Section2/Question1.java
@@ -0,0 +1,17 @@
+package Section2;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] list = new int[4];
+		   for (int i = 0; i < list.length; i++)
+		   list[i] = 3 - i;
+		   System.out.println(list[1]+2);
+		   System.out.println(list[1+2]);
+		   System.out.println(list[1]+list[2]);
+
+
+	}
+
+}
