611ab26b1923faa03918964ea3895950b7f99f3b
Gurpreet Singh <74424705+Gurpreet17@users.noreply.github.com>
2021-08-01 14:04:32 -0400

    Add files via upload

---
+++ Unit  4 - Data Structures/Exercise 1 - Arrays/Question1.java
@@ -0,0 +1,12 @@
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] marks = new int[20];
+		System.out.println(marks[0]);
+		
+
+	}
+
+}
---
+++ Unit  4 - Data Structures/Exercise 1 - Arrays/Question5.java
@@ -0,0 +1,20 @@
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] mass = {0,1,4,6.9,9,10.8,12,14,16};
+		//PART A
+		System.out.println(mass[2]);
+		
+		//PART B
+		System.out.println(mass[5]);
+		
+		//PART D
+	    System.out.println(mass[3]);
+	    
+	    //PART F
+	    System.out.println(mass.length);
+	}
+
+}
